3b9b5c5f4a0151c225b8ce1ffa1083611fc17b81
Ion Sora <soraioniosif@gmail.com>
2021-12-15 19:22:11 +0100

    acabado layout app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
      <h1>To do List</h1>
+     <p>To do tasks: </p>
+     <form>
+     <input type='text'>
+     </input>
+     <button>Add task</button>
+     </form>
+
     </div>
   );
 }
